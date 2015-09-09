8d757bcea5abcbc77aabebd807cb0e556664ad45
Loral <loral_godfrey@yahoo.com>
2015-09-08 23:55:16 -0600

    Changing back to old way

--- ohboymusic.py
+++ ohboymusic.py
@@ -7,61 +7,63 @@ import time
 
 onpi = True
 
+numpins = 6
+
+# switch between piano and guitar every 3 minutes
+seconds = 3 * 60
+
+previnputs = [False for a in range(0, numpins)]
+
+if onpi:
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+
 pygame.mixer.pre_init(channels=6, buffer=1024)
 pygame.mixer.init()
 
-class PianoStairs():
+# 1 2 3   5 6    8
+# g a b c d e f# g
+# g a b   d e    g
+# c d e f g a b  c
+# 8 6 5 3 2 1
+letters = ["d", "e", "f", "g", "a", "b"]
+letters = letters[::-1]
+piano_notes = [pygame.mixer.Sound("piano-notes/"+letter+".wav") for letter in letters]
+guit_let = ["e", "a", "d", "g", "b", "e2"]
+guitar_notes = [pygame.mixer.Sound("guitar/"+letter+".wav") for letter in guit_let]
 
-    def __init__(self):
-        self.numpins = 6
-        # switch between piano and guitar every 3 minutes
-        self.seconds = 3 * 60
 
-        self.previnputs = [False] * self.numpins
 
-        if onpi == True:
-            self.ser = serial.Serial('/dev/ttyACM0', 9600)
-        letters = ["d", "e", "f", "g", "a", "b"]
-        letters = letters[::-1]
-        self.piano_notes = [pygame.mixer.Sound("piano-notes/"+letter+".wav") for letter in letters]
-        guit_let = ["e", "a", "d", "g", "b", "e2"]
-        self.guitar_notes = [pygame.mixer.Sound("guitar/"+letter+".wav") for letter in guit_let]
+def piano(i):
+    piano_notes[i].play()
 
-    def piano(self, i):
-        self.piano_notes[i].play()
+def guitar(i):
+    guitar_notes[i].play()
 
-    def guitar(self, i):
-        self.guitar_notes[i].play()
+count = 0
+playguit = False
 
-    def run(self):
-        count = 0
-        playguit = False
+time.sleep(3)
 
-        time.sleep(3)
+while True:
+    count += 1
+    if count % (20 * seconds) == 0:
+        playguit = not playguit
+    line = ""
+    if onpi:
+        line = ser.readline()
+    else:
+        line = raw_input()
+    if len(line) < 6:
+        continue
+    #line = line[::-1]
+    for i in range(0, numpins):
+        curr = line[i] != '0'
+        prev = previnputs[i]
+        if curr and not prev:
+            piano(i)
 
-        while True:
-            count += 1
-            if count % (20 * self.seconds) == 0:
-                playguit = not playguit
-            line = ""
-            if onpi:
-                line = self.ser.readline()
-            else:
-                line = raw_input()
-            if len(line) < 6:
-                continue
-
-            for i in range(self.numpins):
-                curr = line[i] != '0'
-                prev = self.previnputs[i]
-                if curr and not prev:
-                    if playguit:
-                        self.guitar(i)
-                    else:
-                        self.piano(i)
-                self.previnputs[i] = curr
-
-if __name__ == "__main__":
-    pianoStairs = PianoStairs()
-    pianoStairs.run()
-
+            # if playguit:
+            #     guitar(i)
+            # else:
+            #     piano(i)
+        previnputs[i] = curr
